6c0758037ab4f72be318664b753adb903b8c8ffe
miguecoder <mmarqzm@gmail.com>
2022-09-06 12:30:32 -0500

    script that prepares a MySQL server for the project:

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,8 @@
---SCRIPT THAT CREATE THE DATABASE hbnb_dev_db
---script that prepares a MySQL server for the project:
+-- SCRIPT THAT CREATE THE DATABASE hbnb_dev_db
+-- script that prepares a MySQL server for the project:
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IS NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY hbnb_dev_pwd;
-GRANT ALL ON hbnb_test_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON perfonmance_schema.* TO 'hbnb_dev'@'localhost';
+DROP USER IF EXISTS 'hbnb_dev'@'localhost';
+CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
