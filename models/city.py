e6d314818161191c339d5e8fb44a269efd23b5a7
miguecoder <mmarqzm@gmail.com>
2022-09-06 22:57:34 -0500

    7, 8 and 9

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@
 
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -14,3 +15,4 @@ class City(BaseModel, Base):
         ForeignKey('states.id'),
         nullable=False
         )
+    places = relationship('Place', backref='cities', cascade='all, delete-orfan')
--- models/place.py
+++ models/place.py
@@ -1,18 +1,39 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
-
+from email.policy import default
+import string
+import models
+from models.review import Review
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 class Place(BaseModel):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = 'places'
+    city_id = Column(string(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(string(60), ForeignKey('users.id'), nullable=False)
+    name = Column(string(128), nullable=False)
+    description = Column(String(1024), nullable=False)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(int, default=0, nullable=False)
+    max_guest = Column(int, default=0, nullable=False)
+    price_by_night = Column(int, default=0, nullable=False)
+    latitude = Column(float, nullable=True)
+    longitude = Column(float, nullable=True)
     amenity_ids = []
+    reviews = relationship('Review', backref='place', cascade='all, delete-orfan')
+
+    @property
+    def reviews(self):
+        """
+        Returns the list of Reviews instances with
+        place_id equals to the current Place.id
+        """
+        reviews = list()
+
+        for _id, review in models.storage.all(Review).items():
+            if review.place_id == self.id:
+                reviews.append(review)
+
+        return (reviews)
