350f45fc69e1673879f1ad6edccc0b4c0252e822
santiago1411 <4547@holbertonschool.com>
2022-09-13 19:50:05 -0500

    flask and html

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-"""
-script that starts a Flask web application
-"""
+"""script that starts a flask web application"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """ hello hbnb """
-    return 'Hello HBNB!'
+@app.route('/', strict_slashes=False)
+def web_hello():
+    return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """ hbnb """
-    return 'HBNB!'
+@app.route('/hbnb', strict_slashes=False)
+def web_hbnb():
+    return "HBNB"
 
 
 if __name__ == '__main__':
